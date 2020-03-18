8860691acfef53bdc0cb2c1ba29bb2ba025d9439
rielleriri <41384988+rielleriri@users.noreply.github.com>
2020-03-18 20:57:04 +0800

    Update settings.py

--- myfyp/settings.py
+++ myfyp/settings.py
@@ -126,7 +126,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Configure Django App for Heroku.
 import django_heroku
