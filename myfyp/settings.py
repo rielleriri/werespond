5fe1851eea4681e3bc71fae7617a5e5703053e00
rielleriri <41384988+rielleriri@users.noreply.github.com>
2020-03-19 11:01:04 +0800

    Update settings.py

--- myfyp/settings.py
+++ myfyp/settings.py
@@ -140,6 +140,3 @@ STATICFILES_DIRS = (
 # import django_heroku
 # django_heroku.settings(locals())
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
