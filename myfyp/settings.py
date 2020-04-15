28f0e14745e5fc7c86c7030f6074584f3f3f09ca
rielleriri <41384988+rielleriri@users.noreply.github.com>
2020-04-15 11:12:31 +0800

    Update settings.py

--- myfyp/settings.py
+++ myfyp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 	'werespond',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -45,6 +46,7 @@ MIDDLEWARE = [
     # 'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
